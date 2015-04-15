26f62d70f07639cb13c0a84fccf10cfd74256743
Simon Crase <simon@greenweaves.nz>
2015-04-15 20:50:47 +1200

    test dowsnload function

--- download.R
+++ download.R
@@ -8,16 +8,22 @@ codes=read.delim("data/codes.csv")
 download.file("http://www.asx.com.au/asx/research/ASXListedCompanies.csv","data/ASXListedCompanies.csv")
 companies=read.table("data/ASXListedCompanies.csv",header=TRUE,sep=",",skip=2)
 
-download.file("http://www.asxhistoricaldata.com/wp-content/uploads/2014.zip","data/2014.zip")
-zipdir <- tempfile()
-dir.create(zipdir)
-unzip("data/2014.zip", exdir=zipdir)
+function download.history<-function(url,year) {
+  short_file_name<-paste(toString(year),".zip",sep="")
+  complete_url<-file.path(url,short_file_name)
+  store_dir<-tempfile()
+  dir.create(store_dir)
+  zip_file_name<-file.path(store_dir,short_file_name)
+  download.file(complete_url,zip_file_name)
+  expand_dir<-file_path(store_dir,"expand")
+  unzip(zip_file_name, exdir=expand_dir)
 
-for (d in list.files(zipdir)) {
-  sub=file.path(zipdir,d)
-  for (f in list.files(sub) ){
-    ff=file.path(sub,f)
-    codes=read.csv(ff,header=FALSE)
-    print (head(codes))
+  for (subdir in list.files(expand_dir)) {
+    subsub=file.path(expand_dir,subdir)
+    for (f in list.files(subsub) ){
+      ff=file.path(sub,f)
+      codes=read.csv(ff,header=FALSE)
+      print (head(codes))
+    }
   }
 }
