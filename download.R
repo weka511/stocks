66369f36a71b64cb28793114e15b960f90a3f0c6
Simon Crase <simon@greenweaves.nz>
2015-04-15 21:25:50 +1200

    download a year to a data table

--- download.R
+++ download.R
@@ -8,22 +8,26 @@ codes=read.delim("data/codes.csv")
 download.file("http://www.asx.com.au/asx/research/ASXListedCompanies.csv","data/ASXListedCompanies.csv")
 companies=read.table("data/ASXListedCompanies.csv",header=TRUE,sep=",",skip=2)
 
-function download.history<-function(url,year) {
+library(data.table)
+download.history<-function(url,year) {
+  result<-list()
   short_file_name<-paste(toString(year),".zip",sep="")
   complete_url<-file.path(url,short_file_name)
   store_dir<-tempfile()
   dir.create(store_dir)
   zip_file_name<-file.path(store_dir,short_file_name)
   download.file(complete_url,zip_file_name)
-  expand_dir<-file_path(store_dir,"expand")
+  expand_dir<-file.path(store_dir,"expand")
   unzip(zip_file_name, exdir=expand_dir)
 
   for (subdir in list.files(expand_dir)) {
     subsub=file.path(expand_dir,subdir)
-    for (f in list.files(subsub) ){
-      ff=file.path(sub,f)
-      codes=read.csv(ff,header=FALSE)
-      print (head(codes))
+    for (f in list.files(subsub)) {
+      ff=file.path(subsub,f)
+      result[[length(result)+1]]<-fread(ff,sep=",",header=FALSE)
     }
   }
+  rbindlist(result)
 }
+
+l<-download.history("http://www.asxhistoricaldata.com/wp-content/uploads",2012)
