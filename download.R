9a33062fc8449ec7cbf8d425059b659c3706d5b0
Simon Crase <simon@greenweaves.nz>
2015-04-16 10:09:10 +1200

    successfuly downloaded single year files 2009-2014

--- download.R
+++ download.R
@@ -1,4 +1,5 @@
-rm(list=ls())
+
+library(data.table)
 
 if(!file.exists("data"))
   dir.create("data")
@@ -7,19 +8,20 @@ if(!file.exists("data/au"))
   dir.create("data/au")
 
 
-download.file("http://www.asx.com.au/programs/ISIN.xls","data/codes.csv")
-codes=read.delim("data/codes.csv")
 
-download.file("http://www.asx.com.au/asx/research/ASXListedCompanies.csv","data/ASXListedCompanies.csv")
-companies=read.table("data/ASXListedCompanies.csv",header=TRUE,sep=",",skip=2)
+# download.file("http://www.asx.com.au/programs/ISIN.xls","data/codes.csv")
+# codes=read.delim("data/codes.csv")
+# 
+# download.file("http://www.asx.com.au/asx/research/ASXListedCompanies.csv","data/ASXListedCompanies.csv")
+# companies=read.table("data/ASXListedCompanies.csv",header=TRUE,sep=",",skip=2)
 
-library(data.table)
+
 
 is.numeric.string<-function(candidate) {
   suppressWarnings(!is.na(as.numeric(candidate)))
 }
 
-download.history<-function(url,year) {
+download.history<-function(year,url="http://www.asxhistoricaldata.com/wp-content/uploads",expand="expand") {
   result<-list()
   short_file_name<-paste(toString(year),".zip",sep="")
   complete_url<-file.path(url,short_file_name)
@@ -27,7 +29,7 @@ download.history<-function(url,year) {
   dir.create(store_dir)
   zip_file_name<-file.path(store_dir,short_file_name)
   download.file(complete_url,zip_file_name)
-  expand_dir<-file.path(store_dir,"expand")
+  expand_dir<-file.path(store_dir,expand)
   unzip(zip_file_name, exdir=expand_dir)
 
   for (subdir in list.files(expand_dir)) {
@@ -42,11 +44,16 @@ download.history<-function(url,year) {
   rbindlist(result)
 }
 
-l<-download.history("http://www.asxhistoricaldata.com/wp-content/uploads",2010)
+rename.history.columns<-function(history.data,new.names=c("Code", "Date", "Open", "High", "Low", "Close", "Volume")) {
+  setnames(history.data,names(history.data),new.names)
+}
+
+
 
 for (year in 2009:2014) {
-  l<-download.history("http://www.asxhistoricaldata.com/wp-content/uploads",year)
+  history.data<-download.history(year)
+  rename.history.columns(history.data)
   short_file<-paste(toString(year),".csv",sep="")
-  write.table(l,file.path("c:\\temp",short_file),sep=",")
+  write.table(history.data,file.path("data/au",short_file),sep=",")
 }
 
