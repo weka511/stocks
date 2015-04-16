99c9a2e631c34c6e4a81e5ed7a8433db92dca51f
Simon Crase <simon@greenweaves.nz>
2015-04-16 17:46:38 +1200

    Added copyright norice

--- download.R
+++ download.R
@@ -1,3 +1,22 @@
+# Download ASX historical data
+#
+# Copyright (C) 2015 Simon Crase
+#
+# simon@greenweaves.nz
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 library(data.table)
 
@@ -66,6 +85,13 @@ for (year in c("2007-2008","2005-2006","2003-2004","2000-2002","1997-1999")) {
   history.data<-download.history(year)
   rename.history.columns(history.data)
   short_file<-paste(toString(year),".csv",sep="")
+  write.table(history.data,file.path("data/au",short_file),sep=",")
+}
+
+for (month in c("2015-January","2015-February")) {
+  history.data<-download.history(month)
+  rename.history.columns(history.data)
+  short_file<-paste(toString(month),".csv",sep="")
   write.table(history.data,file.path("data/au",short_file),sep=",")
 }
 
